2d1cb2439b925fdb0a9c42d464226f23e7d283b6
Alaa-Khaled210 <alaa.khaled.ramadan@gmail.com>
2023-04-13 02:14:40 +0200

    lab_3 Q.4 reverse the string

---
+++ Lab_3/reverse_string_array/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char str[50];
+    scanf( "%s", &str);
+    printf (" \n After the reverse of a string: %s \n ", strrev(str));
+    return 0;
+}

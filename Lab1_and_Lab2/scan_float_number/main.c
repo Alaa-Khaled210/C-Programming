15c7f080dc6fb75561f024334a421845059acf68
Alaa Khaled <127328704+Alaa-Khaled210@users.noreply.github.com>
2023-04-14 00:16:40 +0200

    Lab1 & Lab2 Q.3
    Program to print a float number entered by the user

---
+++ Lab1_and_Lab2/scan_float_number/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float num1;
+    printf("Enter a float : \t");
+    scanf("%f", &num1);
+    printf("float number = %f \n", num1);
+    return 0;
+}

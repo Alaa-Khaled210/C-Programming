07b4b886ab8a7865484e5bcb2986f9945decd904
Alaa Khaled <127328704+Alaa-Khaled210@users.noreply.github.com>
2023-04-14 00:18:38 +0200

    Lab1 & Lab2 Q.4
    Program to sum two integers

---
+++ Lab1_and_Lab2/sum_two_number_int/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1,num2,sum;
+    printf("Enter number1 : ");
+    scanf("%d", &num1);
+    printf("Enter number2 : ");
+    scanf("%d", &num2);
+    sum=num1+num2;
+    printf("sum = %d \n",sum );
+
+    return 0;
+}

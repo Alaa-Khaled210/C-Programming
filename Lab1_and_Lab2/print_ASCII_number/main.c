c83f0b09b48d2c4c67e0815f7542b22a9517627f
Alaa Khaled <127328704+Alaa-Khaled210@users.noreply.github.com>
2023-04-14 00:14:56 +0200

    Lab1 & Lab2 Q.2
    Program to print ASCII number of a given char

---
+++ Lab1_and_Lab2/print_ASCII_number/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //---Q.2 Program to print ASCII number of a given char------
+    char x;
+    printf("Enter a char : \t");
+    scanf("%c",&x);
+    printf("ascii for your char is : %d \n",x);
+
+    return 0;
+}
